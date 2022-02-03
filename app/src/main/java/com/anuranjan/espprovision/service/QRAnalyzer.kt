d2689d87738407d3632a3c20514f362addf82f6b
Anuranjan Srivastava <anuranjan.mail@gmail.com>
2022-02-03 17:44:48 +0530

    Created sample code for QR Analyzer using zxing and CameraX

---
+++ app/src/main/java/com/anuranjan/espprovision/service/QRAnalyzer.kt
@@ -0,0 +1,52 @@
+package com.anuranjan.espprovision.service
+
+import android.graphics.ImageFormat
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+import com.google.zxing.*
+import com.google.zxing.common.HybridBinarizer
+import java.nio.ByteBuffer
+
+class QRAnalyzer(private val onQrCodeScanned: (String) -> Unit) : ImageAnalysis.Analyzer {
+
+    private val supportedImageFormatList = listOf(
+        ImageFormat.YUV_420_888,
+        ImageFormat.YUV_422_888,
+        ImageFormat.YUV_444_888
+    )
+
+    override fun analyze(image: ImageProxy) {
+        if (image.format in supportedImageFormatList) {
+            val rawData = image.planes.first().buffer.toByteArray()
+
+            val source = PlanarYUVLuminanceSource(
+                rawData,
+                image.width,
+                image.height,
+                0,
+                0,
+                image.width,
+                image.height,
+                false
+            )
+            val binaryBitmap = BinaryBitmap(HybridBinarizer(source))
+
+            try {
+                val result = MultiFormatReader().apply {
+                    setHints(mapOf(DecodeHintType.POSSIBLE_FORMATS to arrayListOf(BarcodeFormat.QR_CODE)))
+                }.decode(binaryBitmap)
+
+                onQrCodeScanned(result.text)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            } finally {
+                image.close()
+            }
+        }
+    }
+
+    private fun ByteBuffer.toByteArray(): ByteArray {
+        rewind()
+        return ByteArray(remaining()).also { get(it) }
+    }
+}
