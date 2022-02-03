2461b2b78d67b811c15a9f87335d94bafc71d6c9
Anuranjan Srivastava <anuranjan.mail@gmail.com>
2022-02-03 17:45:33 +0530

    Created sample ESP Scanner screen

---
+++ app/src/main/java/com/anuranjan/espprovision/presentation/esp_scanner_screen/EspScannerScreen.kt
@@ -0,0 +1,17 @@
+package com.anuranjan.espprovision.presentation.esp_scanner_screen
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview
+fun EspScannerScreen() {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Text(text = "Not Implemented", modifier = Modifier.align(Alignment.Center))
+    }
+}
