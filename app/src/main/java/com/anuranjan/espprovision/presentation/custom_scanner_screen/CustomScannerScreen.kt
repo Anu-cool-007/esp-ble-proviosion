f01b2f51d2dfee70a0bdce9b53e8d203732e2461
Anuranjan Srivastava <anuranjan.mail@gmail.com>
2022-02-03 21:08:44 +0530

    Moved QR scanner composable to different screen

--- app/src/main/java/com/anuranjan/espprovision/presentation/custom_scanner_screen/CustomScannerScreen.kt
+++ app/src/main/java/com/anuranjan/espprovision/presentation/custom_scanner_screen/CustomScannerScreen.kt
@@ -1,11 +1,6 @@
 package com.anuranjan.espprovision.presentation.custom_scanner_screen
 
 import android.Manifest
-import androidx.camera.core.CameraSelector
-import androidx.camera.core.ImageAnalysis
-import androidx.camera.core.Preview
-import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.camera.view.PreviewView
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -13,15 +8,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.viewinterop.AndroidView
-import androidx.core.content.ContextCompat
 import com.anuranjan.espprovision.presentation.common.RequestPermsWrapper
-import com.anuranjan.espprovision.service.QRAnalyzer
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 
 @ExperimentalPermissionsApi
@@ -34,51 +24,13 @@ fun CustomScannerScreen() {
         Manifest.permission.ACCESS_FINE_LOCATION
     )
 
-    val lifecycleOwner = LocalLifecycleOwner.current
-    val context = LocalContext.current
-
     var qrCode by remember { mutableStateOf("") }
-    val cameraProviderFuture = remember {
-        ProcessCameraProvider.getInstance(context)
-    }
 
-    Column(modifier = Modifier.fillMaxSize()) {
-        RequestPermsWrapper(permissionList = permissionList) {
-            AndroidView(
-                factory = { context ->
-                    val previewView = PreviewView(context)
-
-                    val preview = Preview.Builder().build()
-
-                    val selector = CameraSelector.Builder()
-                        .requireLensFacing(CameraSelector.LENS_FACING_BACK)
-                        .build()
-                    preview.setSurfaceProvider(previewView.surfaceProvider)
-
-                    val imageAnalysis = ImageAnalysis.Builder()
-                        .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
-                        .build()
-                    imageAnalysis.setAnalyzer(
-                        ContextCompat.getMainExecutor(context),
-                        QRAnalyzer { result ->
-                            qrCode = result
-                        }
-                    )
-                    try {
-                        cameraProviderFuture.get().bindToLifecycle(
-                            lifecycleOwner,
-                            selector,
-                            preview,
-                            imageAnalysis
-                        )
-                    } catch (e: Exception) {
-                        e.printStackTrace()
-                    }
-
-                    previewView
-                },
-                modifier = Modifier.weight(1f)
-            )
+    RequestPermsWrapper(permissionList = permissionList) {
+        Column(modifier = Modifier.fillMaxSize()) {
+            QRScanner(modifier = Modifier.weight(1f)) { result ->
+                qrCode = result
+            }
             Text(
                 text = qrCode,
                 fontSize = 20.sp,
---
+++ app/src/main/java/com/anuranjan/espprovision/presentation/custom_scanner_screen/QRScanner.kt
@@ -0,0 +1,60 @@
+package com.anuranjan.espprovision.presentation.custom_scanner_screen
+
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
+import com.anuranjan.espprovision.service.QRAnalyzer
+
+@Composable
+fun QRScanner(modifier: Modifier, onCodeScanned: (String) -> Unit) {
+
+    val lifecycleOwner = LocalLifecycleOwner.current
+    val currentContext = LocalContext.current
+    val cameraProviderFuture = remember {
+        ProcessCameraProvider.getInstance(currentContext)
+    }
+    AndroidView(
+        factory = { context ->
+            val previewView = PreviewView(context)
+
+            val preview = Preview.Builder().build()
+
+            val selector = CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build()
+            preview.setSurfaceProvider(previewView.surfaceProvider)
+
+            val imageAnalysis = ImageAnalysis.Builder()
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .build()
+            imageAnalysis.setAnalyzer(
+                ContextCompat.getMainExecutor(context),
+                QRAnalyzer { result ->
+                    onCodeScanned(result)
+                }
+            )
+            try {
+                cameraProviderFuture.get().bindToLifecycle(
+                    lifecycleOwner,
+                    selector,
+                    preview,
+                    imageAnalysis
+                )
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+
+            previewView
+        },
+        modifier = modifier
+    )
+}
