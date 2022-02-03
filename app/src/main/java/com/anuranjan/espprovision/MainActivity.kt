6370cb76dfd458dba32f80ae06b0c7cd87bfd53b
Anuranjan Srivastava <anuranjan.mail@gmail.com>
2022-02-03 17:43:21 +0530

    Added navigation to main activity

---
+++ app/src/main/java/com/anuranjan/espprovision/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.anuranjan.espprovision
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Size
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
+import androidx.navigation.compose.rememberNavController
+import com.anuranjan.espprovision.presentation.NavConfig
+import com.anuranjan.espprovision.presentation.Screen
+import com.anuranjan.espprovision.ui.theme.ESPProvisionTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ESPProvisionTheme {
+                // A surface container using the 'background' color from the theme
+
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    val navController = rememberNavController()
+                    NavConfig(navController = navController, firstScreen = Screen.HomeScreen)
+                }
+            }
+        }
+    }
+}
