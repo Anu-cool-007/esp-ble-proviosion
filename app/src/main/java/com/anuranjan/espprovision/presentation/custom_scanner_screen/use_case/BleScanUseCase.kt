27483b91d1ef532f155c5ff8fe3ffa6c04acafe9
Anuranjan Srivastava <anuranjan.mail@gmail.com>
2022-02-06 22:35:09 +0530

    Updated BLE usecase to register bt device and service uuid

--- app/src/main/java/com/anuranjan/espprovision/presentation/custom_scanner_screen/use_case/BleScanUseCase.kt
+++ app/src/main/java/com/anuranjan/espprovision/presentation/custom_scanner_screen/use_case/BleScanUseCase.kt
@@ -2,7 +2,9 @@ package com.anuranjan.espprovision.presentation.custom_scanner_screen.use_case
 
 import android.annotation.SuppressLint
 import android.bluetooth.BluetoothDevice
+import android.text.TextUtils
 import com.anuranjan.espprovision.common.Resource
+import com.anuranjan.espprovision.model.BleScanResult
 import com.anuranjan.espprovision.service.BLEScanListener
 import com.espressif.provisioning.ESPProvisionManager
 import kotlinx.coroutines.channels.awaitClose
@@ -12,33 +14,51 @@ import javax.inject.Inject
 
 class BleScanUseCase @Inject constructor() {
     @SuppressLint("MissingPermission")
-    operator fun invoke(espProvisionManager: ESPProvisionManager): Flow<Resource<MutableList<BluetoothDevice?>>> =
-        callbackFlow {
-            trySend(Resource.Loading())
+    operator fun invoke(
+        devicePrefix: String = "",
+        espProvisionManager: ESPProvisionManager
+    ): Flow<Resource<BleScanResult>> = callbackFlow {
+        trySend(Resource.Loading())
 
-            val scannedDevices: MutableList<BluetoothDevice?> =
-                emptyList<BluetoothDevice?>().toMutableList()
+        var isDeviceFound = false
 
-            val bleScanListener = BLEScanListener(
-                listenFailure = { e ->
-                    trySend(Resource.Error(appException = e))
-                    close(e)
-                    Unit
-                },
-                listenScanCompleted = {
-                    trySend(Resource.Success(scannedDevices))
-                    Unit
-                },
-                listenScanStartFailed = {
-                    trySend(Resource.Error(message = "BLE scan failed to start"))
-                    close()
-                    Unit
-                },
-                listenPeripheralFound = { bluetoothDevice, _ ->
-                    scannedDevices.add(bluetoothDevice)
+        val bleScanListener = BLEScanListener(
+            listenFailure = { e ->
+                trySend(Resource.Error(appException = e))
+                close(e)
+                Unit
+            },
+            listenScanCompleted = {
+                    trySend(Resource.Success(data = BleScanResult.BLE_SCAN_FINISHED))
+                Unit
+            },
+            listenScanStartFailed = {
+                trySend(Resource.Error(message = "BLE scan failed to start"))
+                close()
+                Unit
+            },
+            listenPeripheralFound = { bluetoothDevice, scanResult ->
+                scanResult?.scanRecord?.let { scanRecord ->
+                    if (!isDeviceFound && bluetoothDevice != null && !TextUtils.isEmpty(scanRecord.deviceName)) {
+                        if (scanRecord.deviceName == espProvisionManager.espDevice.deviceName) {
+                            // Device found
+                            isDeviceFound = true
+                            var serviceUuid = ""
+                            if (scanRecord.serviceUuids != null && scanRecord.serviceUuids.size > 0) {
+                                serviceUuid = scanRecord.serviceUuids[0].toString()
+                            }
+                            espProvisionManager.espDevice.bluetoothDevice = bluetoothDevice
+                            espProvisionManager.espDevice.primaryServiceUuid = serviceUuid
+                            espProvisionManager.espDevice.connectToDevice()
+
+                            espProvisionManager.stopBleScan()
+                            trySend(Resource.Success(data = BleScanResult.DEVICE_SCANNED))
+                        }
+                    }
                 }
-            )
-            espProvisionManager.searchBleEspDevices(bleScanListener)
-            awaitClose { espProvisionManager.stopBleScan() }
-        }
+            }
+        )
+        espProvisionManager.searchBleEspDevices(devicePrefix, bleScanListener)
+        awaitClose { espProvisionManager.stopBleScan() }
+    }
 }
