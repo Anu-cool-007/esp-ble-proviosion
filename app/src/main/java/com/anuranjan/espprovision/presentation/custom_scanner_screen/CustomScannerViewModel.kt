d64c72f08f5287dd453353ac2a97609a2a62f4c8
Anuranjan Srivastava <anuranjan.mail@gmail.com>
2022-02-06 17:18:56 +0530

    view model for custom scan has state for ble scan and provisioning. It starts scanning BLE after scan

--- app/src/main/java/com/anuranjan/espprovision/presentation/custom_scanner_screen/CustomScannerViewModel.kt
+++ app/src/main/java/com/anuranjan/espprovision/presentation/custom_scanner_screen/CustomScannerViewModel.kt
@@ -1,19 +1,65 @@
 package com.anuranjan.espprovision.presentation.custom_scanner_screen
 
+import android.bluetooth.BluetoothDevice
+import android.content.Context
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.anuranjan.espprovision.common.Resource
 import com.anuranjan.espprovision.model.ProvPayload
+import com.anuranjan.espprovision.presentation.custom_scanner_screen.state.BleScanState
+import com.anuranjan.espprovision.presentation.custom_scanner_screen.state.ProvState
+import com.anuranjan.espprovision.presentation.custom_scanner_screen.state.ScannerState
+import com.anuranjan.espprovision.presentation.custom_scanner_screen.use_case.BleScanUseCase
+import com.espressif.provisioning.ESPProvisionManager
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import javax.inject.Inject
 
 @HiltViewModel
-class CustomScannerViewModel @Inject constructor (): ViewModel() {
-    private val _state = mutableStateOf(CustomScannerState())
+class CustomScannerViewModel @Inject constructor(
+    private val bleScanUseCase: BleScanUseCase,
+    @ApplicationContext context: Context,
+) : ViewModel() {
+    private val espProvisionManager = ESPProvisionManager.getInstance(context)
 
-    val state: State<CustomScannerState> = _state
+    private val _cameraState = mutableStateOf(ScannerState())
+    val cameraState: State<ScannerState> = _cameraState
+
+    private val _bleScanState = mutableStateOf(BleScanState())
+    val bleScanState: State<BleScanState> = _bleScanState
+
+    private val _provState = mutableStateOf(ProvState())
+    val provState: State<ProvState> = _provState
 
     fun setProvPayload(provPayload: ProvPayload) {
-        _state.value = CustomScannerState(isQRCodeScanned = true, provPayload = provPayload)
+        _cameraState.value = ScannerState(isQRCodeScanned = true, provPayload = provPayload)
+        startBleScan()
+    }
+
+    private fun startBleScan() {
+        bleScanUseCase(espProvisionManager).onEach { result ->
+            when (result) {
+                is Resource.Error -> {
+                    _bleScanState.value = BleScanState(
+                        isBleScanFailed = true,
+                        errMessage = result.message ?: "BLE scan failed"
+                    )
+                }
+                is Resource.Loading -> {
+                    _bleScanState.value = BleScanState(isBleScanning = true)
+                }
+                is Resource.Success -> {
+                    _bleScanState.value = BleScanState(
+                        isScanComplete = true,
+                        scannedDevices = result.data
+                            ?: emptyList<BluetoothDevice?>().toMutableList()
+                    )
+                }
+            }
+        }.launchIn(viewModelScope)
     }
 }
